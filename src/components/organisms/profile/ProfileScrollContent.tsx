66c4248b4d6e00409407c5b30d8c8f0e84d1b204
princeofverry <verrykurniawan@students.undip.ac.id>
2025-09-25 20:22:12 +0700

    feat(profile): right content and scroll (but not done yet

---
+++ src/components/organisms/profile/ProfileScrollContent.tsx
@@ -0,0 +1,15 @@
+import CardListPost from "@/components/molecules/card/CardListPost";
+
+export default function ProfileScrollContent() {
+  return (
+    <div className="my-4 flex flex-col gap-6">
+      <CardListPost />
+      <CardListPost />
+      <CardListPost />
+      <CardListPost />
+      <CardListPost />
+      <CardListPost />
+      <CardListPost />
+    </div>
+  );
+}
