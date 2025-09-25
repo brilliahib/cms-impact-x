228d092a3f5adf8ed998d4c22fb3a7ccf6b3cf4b
princeofverry <verrykurniawan@students.undip.ac.id>
2025-09-25 07:44:54 +0700

    feat: profile (not already in responsive)

--- src/components/molecules/card/CardProfile.tsx
+++ src/components/molecules/card/CardProfile.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { Download } from "lucide-react";
 import Image from "next/image";
 
 const CardProfile = () => {
   return (
     <Card className="w-3/4 overflow-hidden p-0">
-      <div className="relative h-40 w-full">
+      <div className="relative h-44 w-full">
         <Image
           src="/images/profile/bg.jpg"
           alt="background"
@@ -13,7 +15,7 @@ const CardProfile = () => {
         />
       </div>
 
-      <div className="relative -mt-16 mb-4 ml-4 flex h-[150px] w-[150px] justify-start rounded-full">
+      <div className="relative -mt-24 ml-4 flex h-[150px] w-[150px] justify-start rounded-full">
         <Image
           src="/images/profile/profile.jpg"
           alt="profile"
@@ -22,28 +24,47 @@ const CardProfile = () => {
         />
       </div>
 
-      <div className="flex flex-row px-6 pb-6">
-        <div>
-          <CardTitle className="text-lg">About</CardTitle>
-          <CardDescription className="mb-4 text-sm tracking-wide">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec
-            leo eget felis pretium fringilla. Mauris ullamcorper, metus non
-            finibus facilisis, urna lacus dapibus eros, vel consequat erat
-            tellus in ante.
+      <div className="-mt-24 flex flex-row items-center gap-6 px-6">
+        <div className="ml-40">
+          <h2 className="font-bold">John Doe</h2>
+          <p className="font-medium text-gray-900/60">UI/UX Designer</p>
+        </div>
+
+        <div className="ml-auto flex gap-2">
+          <Button>Follow</Button>
+          <Button className="bg-white text-black shadow-sm shadow-black hover:bg-gray-300">
+            <Download />
+            Download Portofolio
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-between gap-6 p-6">
+        {/* About Section */}
+        <div className="flex-1">
+          <CardTitle className="pb-4 text-lg">About</CardTitle>
+          <CardDescription className="mb-4 tracking-wider">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+            aperiam eius magnam consequuntur nisi fuga reprehenderit maxime cum
+            possimus doloremque voluptatum expedita sequi nihil exercitationem
+            alias, non adipisci sapiente. Tenetur neque cupiditate optio
+            recusandae modi nam. Amet doloribus impedit reprehenderit ex quas,
+            excepturi, ipsum doloremque praesentium consequatur esse beatae
+            accusamus.
           </CardDescription>
         </div>
 
-        <div className="flex flex-row items-center gap-3">
+        <div className="flex basis-1/5 flex-row items-center gap-1">
           <Image
             src="/images/profile/undip.png"
-            width={80}
-            height={80}
+            width={60}
+            height={60}
             alt="undip"
             className="rounded-md"
           />
-          <div>
-            <h2 className="text-sm font-bold">Universitas Diponegoro</h2>
-            <p className="text-xs">Teknik Komputer</p>
+          <div className="flex flex-col">
+            <h2 className="text-sm font-semibold">Universitas Diponegoro</h2>
+            <p className="text-xs">S1 Teknik Komputer</p>
           </div>
         </div>
       </div>
