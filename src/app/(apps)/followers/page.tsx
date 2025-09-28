77a479fdbaafde3dafa60494ad7db71e9519cb0c
princeofverry <verrykurniawan@students.undip.ac.id>
2025-09-28 16:22:00 +0700

    feat(followers) endpoint followers done

---
+++ src/app/(apps)/followers/page.tsx
@@ -0,0 +1,11 @@
+import FollowersWrapper from "@/components/organisms/followers/FollowersWrapper";
+
+export default function Followers() {
+  return (
+    <>
+      <section>
+        <FollowersWrapper />
+      </section>
+    </>
+  );
+}
