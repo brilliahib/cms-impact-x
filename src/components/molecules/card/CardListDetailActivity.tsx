6aec9390e5c963ba3d9c175ef2ad1fdef26950eb
princeofverry <verrykurniawan@students.undip.ac.id>
2025-09-29 12:30:29 +0700

    feat(activity/detail) detail activity done

--- src/components/molecules/card/CardListDetailActivity.tsx
+++ src/components/molecules/card/CardListDetailActivity.tsx
@@ -13,7 +13,7 @@ export default function CardListDetailActivity() {
           <Card className="hover:bg-background rounded-none! border-0 border-t border-b py-4 shadow-none">
             <CardContent className="flex justify-between gap-4">
               <div className="flex flex-col">
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row items-center gap-2">
                   <Image
                     src={"/images/profile/profile-2d.png"}
                     alt="Profile Image"
@@ -49,7 +49,7 @@ export default function CardListDetailActivity() {
           <Card className="hover:bg-background rounded-none! border-0 border-t border-b py-4 shadow-none">
             <CardContent className="flex justify-between gap-4">
               <div className="flex flex-col">
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row items-center gap-2">
                   <Image
                     src={"/images/profile/profile-2d.png"}
                     alt="Profile Image"
@@ -85,43 +85,7 @@ export default function CardListDetailActivity() {
           <Card className="hover:bg-background rounded-none! border-0 border-t border-b py-4 shadow-none">
             <CardContent className="flex justify-between gap-4">
               <div className="flex flex-col">
-                <div className="flex flex-row gap-2">
-                  <Image
-                    src={"/images/profile/profile-2d.png"}
-                    alt="Profile Image"
-                    width={50}
-                    height={50}
-                  />
-                  <div className="flex flex-col gap-1 text-xs">
-                    <p className="font-medium">Bagus Tri Atmojo</p>
-                    <div className="flex flex-row gap-1">
-                      <p>UI/UX Designer</p>
-                      <span className="opacity-30">|</span>
-                      <p>Universitas Diponegoro</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="ml-14 flex flex-row gap-2">
-                  <Button variant={"default"} size={"sm"}>
-                    <Check />
-                    Approve
-                  </Button>
-                  <Button
-                    variant={"outline"}
-                    size={"sm"}
-                    className="text-red-600"
-                  >
-                    <X color="red" />
-                    Decline
-                  </Button>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className="hover:bg-background rounded-none! border-0 border-t border-b py-4 shadow-none">
-            <CardContent className="flex justify-between gap-4">
-              <div className="flex flex-col">
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row items-center gap-2">
                   <Image
                     src={"/images/profile/profile-2d.png"}
                     alt="Profile Image"
