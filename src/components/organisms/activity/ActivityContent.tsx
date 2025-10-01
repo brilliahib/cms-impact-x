b601fc320bae3102b3132e82c30d36bfd7f970d0
brilliahib <brilliahib21@gmail.com>
2025-10-01 12:21:40 +0700

    fix(activity): validator create activity must be array

--- src/components/organisms/activity/ActivityContent.tsx
+++ src/components/organisms/activity/ActivityContent.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import CardDetailActivity from "@/components/molecules/card/CardDetailActivity";
 import CardListActivity from "@/components/molecules/card/CardListActivity";
+import { useGetAllActivity } from "@/http/activity/get-all-activity";
+import { useSession } from "next-auth/react";
 
 export default function ActivityContent() {
+  const { data: session, status } = useSession();
+
+  const { data, isPending } = useGetAllActivity(
+    session?.access_token as string,
+    {
+      enabled: status === "authenticated",
+    },
+  );
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-      <CardListActivity />
+      <CardListActivity data={data?.data} isPending={isPending} />
       <CardDetailActivity />
     </div>
   );
