710d6793863b379f1575a087779e97e403826745
Bagus Tri Atmojo <100054889+bbagustrm@users.noreply.github.com>
2025-10-01 17:11:15 +0700

    feat: add card recomendation activity

--- src/components/organisms/followers/FollowersRightContent.tsx
+++ src/components/organisms/followers/FollowersRightContent.tsx
@@ -1,14 +1,24 @@
-import CardRecomendationCompetition from "@/components/molecules/card/CardRecomendationCompetition";
-import CardRecomendationProject from "@/components/molecules/card/CardRecomendationProject";
-import CardRecomendationVolunteer from "@/components/molecules/card/CardRecomendationVolunteer";
+import CardRecomendation from "@/components/molecules/card/CardRecomendation";
 
 export default function FollowersRightContent() {
   return (
     <>
       <div className="hidden flex-col gap-6 md:flex">
-        <CardRecomendationProject />
-        <CardRecomendationCompetition />
-        <CardRecomendationVolunteer />
+        <CardRecomendation
+          type="project"
+          title="Recomendation Project"
+          description="Build your portfolio by working on projects."
+        />
+        <CardRecomendation
+          type="competition"
+          title="Recomendation Competition"
+          description="Showcase your skills by competing with others."
+        />
+        <CardRecomendation
+          type="volunteer"
+          title="Recomendation Volunteer"
+          description="Join social activities to make a real impact."
+        />
       </div>
     </>
   );
--- src/components/organisms/home/HomeRightContent.tsx
+++ src/components/organisms/home/HomeRightContent.tsx
@@ -1,15 +1,25 @@
 import CardPeopleSuggest from "@/components/molecules/card/CardPeopleSuggest";
-import CardRecomendationCompetition from "@/components/molecules/card/CardRecomendationCompetition";
-import CardRecomendationProject from "@/components/molecules/card/CardRecomendationProject";
-import CardRecomendationVolunteer from "@/components/molecules/card/CardRecomendationVolunteer";
+import CardRecomendation from "@/components/molecules/card/CardRecomendation";
 
 export default function HomeRightContent() {
   return (
     <div className="flex hidden flex-col gap-6 md:flex">
       <CardPeopleSuggest />
-      <CardRecomendationProject />
-      <CardRecomendationCompetition />
-      <CardRecomendationVolunteer />
+      <CardRecomendation
+        type="project"
+        title="Recomendation Project"
+        description="Build your portfolio by working on projects."
+      />
+      <CardRecomendation
+        type="competition"
+        title="Recomendation Competition"
+        description="Showcase your skills by competing with others."
+      />
+      <CardRecomendation
+        type="volunteer"
+        title="Recomendation Volunteer"
+        description="Join social activities to make a real impact."
+      />
     </div>
   );
 }
